ef3176348f0fc24d6d976fb948c357a4e5f7e26c
Meghana V <meghanameghana8006@gmail.com>
2026-07-18 23:21:15 +0530

    added dashboard files

---
+++ dashboard/dashboard.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", () => {
+
+    const loader =
+        document.getElementById("loader");
+
+    if (loader) {
+
+        setTimeout(() => {
+
+            loader.style.display = "none";
+
+        }, 1000);
+
+    }
+
+});
+
+// ---------- Initialize Dashboard ----------
+
+render();
+
+updateSummary();
+
+updateCharts();
+
+updateBudget();
+
+updateHealth();
+
+updateGoals();
+
+updateInsights();
