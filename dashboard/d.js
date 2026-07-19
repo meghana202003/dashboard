d998ac8121cd27260baa3e1a69df0982b99c0ef2
Meghana V <meghanameghana8006@gmail.com>
2026-07-19 23:27:46 +0530

    added files

---
+++ dashboard/d.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+
+    const loader =
+        document.getElementById("loader");
+
+    if (loader) {
+
+        setTimeout(() => {
+
+            loader.style.display = "none";
+
+        }, 1000);
+
+    }
