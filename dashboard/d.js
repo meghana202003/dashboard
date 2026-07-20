eb0a65b2e84da11610a8f47ce9cf3a27ad9fb478
Meghana V <meghanameghana8006@gmail.com>
2026-07-20 21:53:48 +0530

    edited files

--- dashboard/d.js
+++ dashboard/d.js
@@ -1,3 +1,10 @@
+const desc = document.querySelector("#desc");
+const amount = document.querySelector("#amount");
+const category = document.querySelector("#category");
+const type = document.querySelector("#type");
+
+const addBtn = document.querySelector("#addBtn");
+const tbody = document.querySelector("#tbody");
 window.addEventListener("load", () => {
 
     const loader =
