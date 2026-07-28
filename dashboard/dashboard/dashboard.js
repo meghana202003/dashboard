d7fea511c5f1fd3bc8e1e7d5d24ec0c98e0f232c
Meghana V <meghanameghana8006@gmail.com>
2026-07-28 21:24:40 +0530

    updated

--- dashboard/dashboard/dashboard.js
+++ dashboard/dashboard/dashboard.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
         document.getElementById("loader"); 
 
     if (loader) {  
-        setTimeout(() => { hfvgj jhd
+        setTimeout(() => { hfvgj jhd hafdgfaj
 
             loader.style.display = "none";
 
