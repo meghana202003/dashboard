840b42e60c1ada7e280fa50df27e1a8713e9dcb4
Meghana V <meghanameghana8006@gmail.com>
2026-07-31 14:41:47 +0530

    updated

--- dashboard/dashboard/dashboard.js
+++ dashboard/dashboard/dashboard.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
         document.getElementById("loader"); 
 
     if (loader) {  
-        setTimeout(() => { djsg jfwif
+        setTimeout(() => { djsg jfwif hj
 
           loader.style.display = "none";
 
