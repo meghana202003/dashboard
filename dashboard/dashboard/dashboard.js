6bc834604c49f6defcdf5d2da2b145853db646c0
Meghana V <meghanameghana8006@gmail.com>
2026-08-04 21:50:54 +0530

    updated

--- dashboard/dashboard/dashboard.js
+++ dashboard/dashboard/dashboard.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
         document.getElementById("loader"); 
 
     if (loader) {  
-        setTimeout(() => { ugkugkgfhfghfkhk git
+        setTimeout(() => { ugkugkgfhfghfkhk gitjrrrrkjrkjS
           loader.style.display = "none";
 
         }, 1000);
