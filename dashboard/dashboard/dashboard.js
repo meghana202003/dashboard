cff38132c04340afc86d7686b7b2167d22411ef5
Meghana V <meghanameghana8006@gmail.com>
2026-08-10 23:09:28 +0530

    updated

--- dashboard/dashboard/dashboard.js
+++ dashboard/dashboard/dashboard.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
         document.getElementById("loader"); 
 
     if (loader) {  
-        setTimeout(() => { huhhgffuyjffgyggy
+        setTimeout(() => { huhhgffuyjffgyggyghfv
           loader.style.display = "none";
 
         }, 1000);
