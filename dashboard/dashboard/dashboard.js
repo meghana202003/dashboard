fa17eede978de43100667c72fdc065c193a9d149
Meghana V <meghanameghana8006@gmail.com>
2026-08-27 22:45:42 +0530

    updated

--- dashboard/dashboard/dashboard.js
+++ dashboard/dashboard/dashboard.js
@@ -4,7 +4,7 @@ window.addEventListener("load", () => {
         document.getElementById("loader"); 
 
     if (loader) {  
-        setTimeout(() => {ndjfngjjrjhkrgkkffgyhhhgggghfhhfhhhhjhhdhggghhhdhhhhjjj
+        setTimeout(() => {ndjfngjjrjhkrgkkffgyhhhgggghfhhfhhhhjhhdjjhggghhhdhhhhjjj
           loader.style.display = "none";
 
         }, 1000);
